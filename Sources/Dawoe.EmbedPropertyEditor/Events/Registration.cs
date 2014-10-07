388e6eb933af6bcb86a22b566791bec08fcaf30d
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2014-10-07 17:54:30 +0200

    Setup routing for embedded resource controller

---
+++ Sources/Dawoe.EmbedPropertyEditor/Events/Registration.cs
@@ -0,0 +1,26 @@
+﻿namespace Dawoe.EmbedPropertyEditor.Events
+{
+    using System.Web.Routing;
+
+    using Umbraco.Core;
+
+    /// <summary>
+    /// Registration of umbraco event handlers
+    /// </summary>
+    internal class Registration : ApplicationEventHandler
+    {
+        /// <summary>
+        /// The application started.
+        /// </summary>
+        /// <param name="umbracoApplication">
+        /// The umbraco application.
+        /// </param>
+        /// <param name="applicationContext">
+        /// The application context.
+        /// </param>
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ Sources/Dawoe.EmbedPropertyEditor/Events/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿namespace Dawoe.EmbedPropertyEditor.Events
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    /// <summary>
+    /// The route config.
+    /// </summary>
+    internal class RouteConfig
+    {
+        /// <summary>
+        /// Register custom routes
+        /// </summary>
+        /// <param name="routes">
+        /// The routes.
+        /// </param>
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute(
+                name: "Dawoe.EmbedPropertyEditor",
+                url: "App_Plugins/DawoeEmbedPropertyEditor/{action}/{filename}",
+                defaults: new { controller = "EmbeddedResource", action = "Resource", filename = UrlParameter.Optional });
+        }
+    }
+}
