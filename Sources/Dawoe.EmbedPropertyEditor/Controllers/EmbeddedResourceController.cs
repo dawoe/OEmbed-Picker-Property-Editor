46ce80bc081d43fa6b5280878b1761bc3bde72b7
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2014-10-07 17:27:04 +0200

    Added embedded resource surface controller

---
+++ Sources/Dawoe.EmbedPropertyEditor/Controllers/EmbeddedResourceController.cs
@@ -0,0 +1,12 @@
+﻿namespace Dawoe.EmbedPropertyEditor.Controllers
+{
+    using Umbraco.Web.Mvc;
+
+    /// <summary>
+    /// The embedded resource controller.
+    /// </summary>
+    [PluginController("Dawoe.EmbedPropertyEditor")]
+    public class EmbeddedResourceController : SurfaceController
+    {
+    }
+}
