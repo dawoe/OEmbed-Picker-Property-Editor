9a47e7364fea48ffd1c513b4ddaa45c303d93198
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2014-10-08 17:02:34 +0200

    Added AllowMultiple setting to prevalue editor

--- Sources/Dawoe.EmbedPropertyEditor/EmbedPrevalueEditor.cs
+++ Sources/Dawoe.EmbedPropertyEditor/EmbedPrevalueEditor.cs
@@ -7,5 +7,11 @@
     /// </summary>
     internal class EmbedPrevalueEditor : PreValueEditor
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether allow multiple.
+        /// </summary>
+        [PreValueField("allowmultiple", "Allow multiple", "checkbox",
+            Description = "Allow the editor pick multiple embeds")]
+        public bool AllowMultiple { get; set; }
     }
 }
