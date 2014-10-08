12e92e68d7e95ecf818188535cef2b46dfd97168
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2014-10-08 16:47:08 +0200

    Added property editor class

---
+++ Sources/Dawoe.EmbedPropertyEditor/EmbedPropertyEditor.cs
@@ -0,0 +1,11 @@
+﻿namespace Dawoe.EmbedPropertyEditor
+{
+    using Umbraco.Core.PropertyEditors;
+
+    /// <summary>
+    /// The embed property editor.
+    /// </summary>
+    public class EmbedPropertyEditor : PropertyEditor
+    {
+    }
+}
