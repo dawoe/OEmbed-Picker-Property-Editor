6e746dff6ad58c8e5e73fc098317723054ab38b0
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2014-10-09 18:09:19 +0200

    Added property value convertor

---
+++ Sources/Dawoe.EmbedPropertyEditor/PropertyValueConvertors/EmbedPropertyValueConvertor.cs
@@ -0,0 +1,46 @@
+﻿namespace Dawoe.EmbedPropertyEditor.PropertyValueConvertors
+{
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    using Newtonsoft.Json;
+
+    using Umbraco.Core.Models.PublishedContent;
+    using Umbraco.Core.PropertyEditors;
+
+    /// <summary>
+    /// The embed property value convertor.
+    /// </summary>
+    [PropertyValueType(typeof(List<MvcHtmlString>))]
+    [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
+    public class EmbedPropertyValueConvertor : PropertyValueConverterBase
+    {
+        /// <summary>
+        /// Checks if this is a convertor for the propety
+        /// </summary>
+        /// <param name="propertyType">
+        /// The property type.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.Equals("Dawoe.EmbedPropertyEditor");
+        }
+
+        public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            var returnValue = new List<MvcHtmlString>();
+
+            if (source == null || string.IsNullOrEmpty(source.ToString()))
+            {                
+                return returnValue;
+            }
+
+            var json = JsonConvert.DeserializeObject(source.ToString());
+
+            return returnValue;
+        }
+    }
+}
