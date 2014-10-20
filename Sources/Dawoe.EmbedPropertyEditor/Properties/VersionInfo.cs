a698a8dc230e5426584566abdfdb2f66c8598055
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2014-10-20 08:34:32 +0200

    Copy dll files to build directory from build process

--- Sources/Dawoe.EmbedPropertyEditor/Properties/VersionInfo.cs
+++ Sources/Dawoe.EmbedPropertyEditor/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.23128")]
+[assembly: AssemblyFileVersion("1.0.0.23377")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
 
 
