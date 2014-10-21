c9bbe62099ad5f897ceb928de69fab7f6cbcee7f
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2014-10-21 19:33:29 +0200

    Update manifest for nuget

--- Sources/Dawoe.EmbedPropertyEditor/Properties/VersionInfo.cs
+++ Sources/Dawoe.EmbedPropertyEditor/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.53136")]
+[assembly: AssemblyFileVersion("1.0.0.53324")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
 
 
