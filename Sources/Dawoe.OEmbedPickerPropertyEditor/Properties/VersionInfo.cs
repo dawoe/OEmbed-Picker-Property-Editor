5c2004e50f0c6442ecb90cccce97151095231b39
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2014-10-24 10:35:30 +0200

    Build package for release

--- Sources/Dawoe.OEmbedPickerPropertyEditor/Properties/VersionInfo.cs
+++ Sources/Dawoe.OEmbedPickerPropertyEditor/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.22297")]
+[assembly: AssemblyFileVersion("1.0.0.27430")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
 
 
