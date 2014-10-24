50c1e987ecb23fbcc0e14814ad82c4be8deeca05
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2014-10-24 08:11:06 +0200

    Apply new name in packaging

--- Sources/Dawoe.OEmbedPickerPropertyEditor/Properties/VersionInfo.cs
+++ Sources/Dawoe.OEmbedPickerPropertyEditor/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.49906")]
+[assembly: AssemblyFileVersion("1.0.0.22297")]
 [assembly: AssemblyInformationalVersion("1.0.0")]
 
 
