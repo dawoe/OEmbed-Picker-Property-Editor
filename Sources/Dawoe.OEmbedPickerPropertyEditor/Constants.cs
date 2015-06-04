ddbcf5386145073b38eb642096e918a880f351d7
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2015-06-04 15:46:47 +0200

    Removed unused constants

--- Sources/Dawoe.OEmbedPickerPropertyEditor/Constants.cs
+++ Sources/Dawoe.OEmbedPickerPropertyEditor/Constants.cs
@@ -8,23 +8,18 @@
         /// <summary>
         /// The property editor alias.
         /// </summary>
-        public const string PropertyEditorAlias = "Dawoe.OEmbedPickerPropertyEditor";
+        internal const string PropertyEditorAlias = "Dawoe.OEmbedPickerPropertyEditor";
 
         /// <summary>
         /// The data type cache key.
         /// </summary>
-        public const string DataTypeCacheKey = "Dawoe.OEmbedPickerPropertyEditor.AllowMultiple_{0}";
+        internal const string DataTypeCacheKey = "Dawoe.OEmbedPickerPropertyEditor.AllowMultiple_{0}";
 
         /// <summary>
         /// The allow multiple prevalue.
         /// </summary>
-        public const string AllowMultiplePrevalue = "allowmultiple";
-
-        /// <summary>
-        /// The resource folder.
-        /// </summary>
-        public const string ResourceFolder = "App_Plugins/DawoeOEmbedPickerPropertyEditor";
-
+        internal const string AllowMultiplePrevalue = "allowmultiple";
+       
         /// <summary>
         /// The plugin root path.
         /// </summary>
