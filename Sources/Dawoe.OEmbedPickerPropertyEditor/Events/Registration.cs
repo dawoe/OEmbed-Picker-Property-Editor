6b57ab26166ed9bced9e5796d55956366b77ae90
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-12-02 13:17:24 +0100

    removed caching of datatype service calls..this should be handled by core

--- Sources/Dawoe.OEmbedPickerPropertyEditor/Events/Registration.cs
+++ Sources/Dawoe.OEmbedPickerPropertyEditor/Events/Registration.cs
@@ -20,37 +20,7 @@
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
         {           
             var clientSetup = new ClientSetup();
-            clientSetup.Configure();            
-            DataTypeService.Saved += this.DataTypeServiceSaved;
-            DataTypeService.Deleted += this.DataTypeServiceDeleted;
-        }
-
-        /// <summary>
-        /// The data type service deleted event handler.
-        /// </summary>
-        /// <param name="sender">
-        /// The sender.
-        /// </param>
-        /// <param name="e">
-        /// The e.
-        /// </param>
-        protected void DataTypeServiceDeleted(IDataTypeService sender, Umbraco.Core.Events.DeleteEventArgs<Umbraco.Core.Models.IDataTypeDefinition> e)
-        {            
-            Caching.RemoveFromDatatypesFromCache(e.DeletedEntities);
-        }
-
-        /// <summary>
-        /// The data type service saved event handler
-        /// </summary>
-        /// <param name="sender">
-        /// The sender.
-        /// </param>
-        /// <param name="e">
-        /// The e.
-        /// </param>
-        protected void DataTypeServiceSaved(IDataTypeService sender, Umbraco.Core.Events.SaveEventArgs<Umbraco.Core.Models.IDataTypeDefinition> e)
-        {
-            Caching.RemoveFromDatatypesFromCache(e.SavedEntities);
-        }
+            clientSetup.Configure();                       
+        }        
     }
 }
--- Sources/Dawoe.OEmbedPickerPropertyEditor/PropertyValueConvertors/OEmbedPickerPropertyValueConvertor.cs
+++ Sources/Dawoe.OEmbedPickerPropertyEditor/PropertyValueConvertors/OEmbedPickerPropertyValueConvertor.cs
@@ -6,8 +6,6 @@
     using System.Web;
     using System.Web.Mvc;
 
-    using Dawoe.OEmbedPickerPropertyEditor.Caching;
-
     using Newtonsoft.Json;
 
     using Umbraco.Core;
