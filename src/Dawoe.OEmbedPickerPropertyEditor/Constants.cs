c7b7748d7dfd65d25e070fe56f0328305211aa50
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-03-18 09:10:34 +0100

    Added a data editor configuration class

---
+++ src/Dawoe.OEmbedPickerPropertyEditor/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Dawoe.OEmbedPickerPropertyEditor
+{
+    /// <summary>
+    /// A object defining all constants used in the project
+    /// </summary>
+    public class Constants
+    {
+        /// <summary>
+        /// The data editor alias.
+        /// </summary>
+        public const string DataEditorAlias = "Dawoe.OEmbedPickerPropertyEditor";
+    }
+}
---
+++ src/Dawoe.OEmbedPickerPropertyEditor/OEmbedPickerDataEditor.cs
@@ -0,0 +1,29 @@
+﻿namespace Dawoe.OEmbedPickerPropertyEditor
+{
+    using Umbraco.Core.Logging;
+    using Umbraco.Core.PropertyEditors;
+
+    /// <summary>
+    /// Represents the OEmbed picker data editor
+    /// </summary>
+    [DataEditor(Constants.DataEditorAlias, "OEmbed Picker", "~/App_Plugins/Dawoe.OEmbedPickerPropertyEditor/views/editor.html", ValueType = "JSON", Group = "pickers", Icon = "icon-tv")]
+    internal class OEmbedPickerDataEditor : DataEditor
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OEmbedPickerDataEditor"/> class.
+        /// </summary>
+        /// <param name="logger">
+        /// The logger.
+        /// </param>
+        /// <param name="type">
+        /// The editor type type.
+        /// </param>
+        public OEmbedPickerDataEditor(ILogger logger, EditorType type = EditorType.PropertyValue)
+            : base(logger, type)
+        {
+        }
+
+        /// <inheritdoc />
+        protected override IConfigurationEditor CreateConfigurationEditor() => new OEmbedPickerConfigurationEditor();
+    }
+}
