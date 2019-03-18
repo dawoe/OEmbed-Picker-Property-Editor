83a0fdb1e31cb70b16f041c270626e78566392a1
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-03-18 09:02:13 +0100

    Added configuration objects for the configuration

---
+++ src/Dawoe.OEmbedPickerPropertyEditor/OEmbedPickerConfiguration.cs
@@ -0,0 +1,16 @@
+﻿namespace Dawoe.OEmbedPickerPropertyEditor
+{
+    using Umbraco.Core.PropertyEditors;
+
+    /// <summary>
+    /// Represents the configuration for the OEmbed picker
+    /// </summary>
+    internal class OEmbedPickerConfiguration
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether multiple items are allowed to be picked
+        /// </summary>
+        [ConfigurationField("allowmultiple", "Allow picking of multiple items", "boolean")]
+        public bool AllowMultiple { get; set; }
+    }
+}
---
+++ src/Dawoe.OEmbedPickerPropertyEditor/OEmbedPickerConfigurationEditor.cs
@@ -0,0 +1,15 @@
+﻿namespace Dawoe.OEmbedPickerPropertyEditor
+{
+    using Umbraco.Core.PropertyEditors;
+
+    /// <summary>
+    /// Represents the configuration editor for the OEmbed picker
+    /// </summary>
+    internal class OEmbedPickerConfigurationEditor : ConfigurationEditor<OEmbedPickerConfiguration>
+    {
+        /// <summary>
+        /// Gets the default configuration object.
+        /// </summary>
+        public override object DefaultConfigurationObject { get; } = new OEmbedPickerConfiguration { AllowMultiple = false };
+    }
+}
