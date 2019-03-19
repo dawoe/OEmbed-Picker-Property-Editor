ab7d73380b274285a12ca48c9c5d8e43b8047032
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-03-19 18:47:23 +0100

    Basic setup of angular part

---
+++ src/Dawoe.OEmbedPickerPropertyEditor.Web/App_Plugins/Dawoe.OEmbedPickerPropertyEditor/scripts/editor.controller.js
@@ -0,0 +1,14 @@
+﻿(function() {
+    'use strict';
+
+    function EditorController($scope, editorService) {
+        var vm = this;
+
+       
+
+
+    }
+
+    angular.module('umbraco').controller('Dawoe.OEmbedPickerEditorController',
+        ['$scope', 'editorService', EditorController]);
+})();
