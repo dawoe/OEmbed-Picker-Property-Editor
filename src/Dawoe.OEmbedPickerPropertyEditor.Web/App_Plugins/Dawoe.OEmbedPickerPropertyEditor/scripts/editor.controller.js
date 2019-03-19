6f898fc9ea4bae5dcb2bba1409c824d3ed17e82a
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-03-19 18:54:52 +0100

    open embed dialog when clikcing add item button

--- src/Dawoe.OEmbedPickerPropertyEditor.Web/App_Plugins/Dawoe.OEmbedPickerPropertyEditor/scripts/editor.controller.js
+++ src/Dawoe.OEmbedPickerPropertyEditor.Web/App_Plugins/Dawoe.OEmbedPickerPropertyEditor/scripts/editor.controller.js
@@ -2,11 +2,22 @@
     'use strict';
 
     function EditorController($scope, editorService) {
-        var vm = this;
+        var vm = this;      
 
-       
+        function openEmbedDialog() {
+            var editorOptions = {
+                submit: function (model) {
+                    editorService.close();
+                },
+                close: function () {
+                    editorService.close();
+                }
+            }
 
+            editorService.embed(editorOptions);
+        }
 
+        vm.open = openEmbedDialog;
     }
 
     angular.module('umbraco').controller('Dawoe.OEmbedPickerEditorController',
