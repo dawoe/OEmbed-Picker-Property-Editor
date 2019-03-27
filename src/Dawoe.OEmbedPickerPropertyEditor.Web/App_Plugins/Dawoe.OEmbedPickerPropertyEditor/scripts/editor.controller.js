dd7280c11df395b04965f009d619765ab4ed72c5
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-03-27 07:52:04 +0100

    UI changes in displaying picked items

--- src/Dawoe.OEmbedPickerPropertyEditor.Web/App_Plugins/Dawoe.OEmbedPickerPropertyEditor/scripts/editor.controller.js
+++ src/Dawoe.OEmbedPickerPropertyEditor.Web/App_Plugins/Dawoe.OEmbedPickerPropertyEditor/scripts/editor.controller.js
@@ -1,12 +1,20 @@
 ﻿(function() {
     'use strict';
 
-    function EditorController($scope, editorService) {
-        var vm = this;      
+    function EditorController($scope, $sce, editorService) {
+        var vm = this;
 
+        vm.items = Array.isArray($scope.model) ? $scope.model : [];
+        
         function openEmbedDialog() {
             var editorOptions = {
                 submit: function (model) {
+                    console.log(model);
+
+                    var item = model.embed;
+
+                    vm.items.push(item);
+
                     editorService.close();
                 },
                 close: function () {
@@ -17,9 +25,15 @@
             editorService.embed(editorOptions);
         }
 
+        function trustHtml(html) {
+            return $sce.trustAsHtml(html);
+        }
+
         vm.open = openEmbedDialog;
+        vm.trustHtml = trustHtml;
+
     }
 
     angular.module('umbraco').controller('Dawoe.OEmbedPickerEditorController',
-        ['$scope', 'editorService', EditorController]);
+        ['$scope', '$sce', 'editorService', EditorController]);
 })();
