974efa62244e142a9617078fa1e5a4479c8084e8
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-04-01 08:28:47 +0200

    Added model to be returned from property value converter

---
+++ src/Dawoe.OEmbedPickerPropertyEditor/Models/OEmbedItem.cs
@@ -0,0 +1,30 @@
+﻿namespace Dawoe.OEmbedPickerPropertyEditor.Models
+{
+    using System.Web;
+
+    /// <summary>
+    /// Represents a item picked in the editor
+    /// </summary>
+    public class OEmbedItem
+    {
+        /// <summary>
+        /// Gets or sets the url.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets the width.
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// Gets or sets the height.
+        /// </summary>
+        public int Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets the preview.
+        /// </summary>
+        public IHtmlString Preview { get; set; }
+    }
+}
