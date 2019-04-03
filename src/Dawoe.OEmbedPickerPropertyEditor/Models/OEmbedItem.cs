6f9905a78c3585c1b486b2a2d103b99feef056dc
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-04-03 08:05:29 +0200

    Added value converter

--- src/Dawoe.OEmbedPickerPropertyEditor/Models/OEmbedItem.cs
+++ src/Dawoe.OEmbedPickerPropertyEditor/Models/OEmbedItem.cs
@@ -2,6 +2,8 @@
 {
     using System.Web;
 
+    using Newtonsoft.Json;
+
     /// <summary>
     /// Represents a item picked in the editor
     /// </summary>
@@ -10,21 +12,25 @@
         /// <summary>
         /// Gets or sets the url.
         /// </summary>
+        [JsonProperty(PropertyName = "url")]
         public string Url { get; set; }
 
         /// <summary>
         /// Gets or sets the width.
         /// </summary>
+        [JsonProperty(PropertyName = "width")]
         public int Width { get; set; }
 
         /// <summary>
         /// Gets or sets the height.
         /// </summary>
+        [JsonProperty(PropertyName = "height")]
         public int Height { get; set; }
 
         /// <summary>
         /// Gets or sets the preview.
         /// </summary>
+        [JsonProperty(PropertyName = "preview")]
         public IHtmlString Preview { get; set; }
     }
 }
