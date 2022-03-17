cf4882244cc9f4d7cb3c4e73f9c5ed800cc5c6e8
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2022-03-17 09:03:12 +0100

    Setup building of nuget packages

---
+++ Packaging/BuildPackage.ps1
@@ -0,0 +1,29 @@
+﻿param($versionSuffix)
+
+
+$CurrentDir = Split-Path $MyInvocation.MyCommand.Path
+$RootDir = Split-Path -Path $CurrentDir -Parent
+$UtilitiesDir = "$RootDir\Utilities"
+$DistDir = "$RootDir\dist"
+
+#Remove dist folder
+if(Test-Path -Path $DistDir -PathType Leaf)
+{
+    Remove-Item -LiteralPath $DistDir -Force -Recurse
+}
+
+#Prepare build
+. $UtilitiesDir\Prepare.ps1 
+
+Prepare-Solution -CurrentDir $UtilitiesDir -Configuration 'Release'
+
+$buildFolder = Join-Path -Path $RootDir -ChildPath 'build';
+
+if(-Not([string]::IsNullOrEmpty($versionSuffix)))
+{
+    dotnet pack $RootDir\src\Dawoe.OEmbedPickerPropertyEditor.sln -c Release -o $DistDir --version-suffix $VersionSuffix --no-build
+}
+else
+{
+    dotnet pack $RootDir\src\Dawoe.OEmbedPickerPropertyEditor.sln -c Release -o $DistDir  --no-build
+}
