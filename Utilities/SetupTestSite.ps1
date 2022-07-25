8bdf8ea2efa962dfbdce32e53ad8027625bdce53
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2022-07-25 13:00:56 +0200

    Updated setup script for creating test site

--- Utilities/SetupTestSite.ps1
+++ Utilities/SetupTestSite.ps1
@@ -1,73 +1,39 @@
-﻿function Create-V8-Site {
+﻿function Create-Test-Site {
     [CmdletBinding()]
 	param(
 		[Parameter()]
-		[string] $RootDir,
-
+		[string] $Destination,  
+        
         [Parameter()]
-		[string] $Destination,
-
-        [Parameter()]
-		[string] $DownloadUrl = 'https://our.umbraco.com/ReleaseDownload?id=276831' 
-	)
-
-    $DownloadDir = "$RootDir\downloads"
-    $ZipFile =  "$DownloadDir\umbraco8.zip"
-
-    if (Test-Path -Path $DownloadDir) {
-        Remove-Item -LiteralPath $DownloadDir -Force -Recurse
-    }
-
-    Write-Host "Creating download directory"
-
-    New-Item -Path $RootDir -Name "downloads" -ItemType "directory"
-
-    Write-Host "Downloading Umbraco 8.1.0 to $ZipFile"
-
-    Invoke-WebRequest -Uri $DownloadUrl -OutFile $ZipFile 
- 
-    Write-Host "Extracting downloaded zip file to $Destination"
-
-    Expand-Archive -LiteralPath $ZipFile -DestinationPath "$Destination\v8" -Force
-
-    Write-Host "Cleaning up downloaded files"
-
-    Remove-Item -LiteralPath $DownloadDir -Force -Recurse
-}
-
-function Create-V9-Site {
-    [CmdletBinding()]
-	param(
-		[Parameter()]
-		[string] $Destination        
+		[string] $ProjectName
 	)
 
     $CurrentDir = Get-Location
 
     Write-Host $CurrentDir
 
-    Write-Host "Installing Umbraco 9 templates"
-    dotnet new --install Umbraco.Templates::9.0.0
+    Write-Host "Installing Umbraco templates"
+    dotnet new --install Umbraco.Templates
 
-    Write-Host "Creating Umbraco 9 site"
+    Write-Host "Creating Umbraco site"
     cd $Destination
-    dotnet new umbraco --SqlCe -n v9
+    dotnet new umbraco -n $ProjectName --development-database-type SQLite --version 10.0.0
 
-    cd "$Destination\v9"
+    cd "$Destination\$ProjectName"
 
-    dotnet add package Umbraco.TheStarterKit --version 9.0.0 --source https://api.nuget.org/v3/index.json
+    dotnet add package Umbraco.TheStarterKit --version 10.0.0 --source https://api.nuget.org/v3/index.json
 
     dotnet build
 
     # load project file xml
     $xml = New-Object System.Xml.XmlDocument
-    $xml.Load("$Destination\v9\v9.csproj")
+    $xml.Load("$Destination\$ProjectName\$ProjectName.csproj")
 
     $propertyGroup = Select-XML -Xml $xml -XPath '//PropertyGroup[1]'
     $newNode = $xml.CreateElement('RestoreAdditionalProjectSources')
     $newNode.InnerText = '../Nuget'
     $propertyGroup.Node.AppendChild($newNode)  
-    $xml.Save("$Destination\v9\v9.csproj")
+    $xml.Save("$Destination\$ProjectName\$ProjectName.csproj")
 
     cd $CurrentDir
 }
@@ -76,6 +42,8 @@ function Create-V9-Site {
 $CurrentDir = Split-Path $MyInvocation.MyCommand.Path
 $RootDir = Split-Path -Path $CurrentDir -Parent
 $Destination= "$RootDir\testsites"
+$TestProjectName = "OEmbedPickerSite"
+$PackageName = "Dawoe.OEmbedPickerPropertyEditor"
 
 Write-Host "Cleaning up existing test site"
 
@@ -85,14 +53,7 @@ if (Test-Path -Path $Destination) {
 
 New-Item -Path $RootDir -Name "testsites" -ItemType "directory"
 
-Create-V8-Site -RootDir $RootDir -Destination $Destination
-
-Create-V9-Site $Destination
-
-. $CurrentDir\Compile.ps1
-
-Compile-Solution -RootDir $RootDir -Configuration Debug
-
+Create-Test-Site $Destination $TestProjectName
 
 Write-Host "Create nuget packages"
 
@@ -100,11 +61,11 @@ $dateTime = get-date -Format "ddMMyyyyHHmmss"
 
 Write-Host "Version suffix $dateTime"
 
-dotnet pack $RootDir\src\Dawoe.OEmbedPickerPropertyEditor.sln -c Debug -o $Destination\nuget --version-suffix "$dateTime" --no-build
+dotnet pack $RootDir\src\$PackageName.sln -c Debug -o $Destination\nuget --version-suffix "$dateTime" --no-build
 
-cd "$Destination\v9"
+cd "$Destination\$TestProjectName"
 
-dotnet add package Dawoe.OEmbedPickerPropertyEditor -v 10.0.0-$dateTime --no-restore
+dotnet add package $PackageName -v 10.0.0-$dateTime --no-restore
 
 dotnet build
 
