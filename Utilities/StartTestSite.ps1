8bd5a23357b6f2c8bf3de8d37b5f0ff30dfe7343
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2022-07-25 13:11:19 +0200

    Updated start test site script

--- Utilities/StartTestSite.ps1
+++ Utilities/StartTestSite.ps1
@@ -1,7 +1,5 @@
 ﻿$CurrentDir = Split-Path $MyInvocation.MyCommand.Path
-$RootDir = Split-Path -Path $CurrentDir -Parent
-$SiteDir= "$RootDir\testsites\v9"
+. $CurrentDir\Variables.ps1
 
 
-
-dotnet watch run --no-restore --project $SiteDir
+dotnet watch run --no-restore --project "$TestSitesFolder\$TestProjectName"
